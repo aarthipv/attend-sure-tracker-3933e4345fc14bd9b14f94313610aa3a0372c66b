59be9af41200d638e42ed0db027f06e1d8073e1d
Aarthi <aarthipv2004@gmail.com>
2025-06-02 11:10:21 +0530

    vercel deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,10 +11,9 @@ export default defineConfig({
     },
   },
   server: {
-    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'http://localhost:5003',
         changeOrigin: true,
       },
     },
